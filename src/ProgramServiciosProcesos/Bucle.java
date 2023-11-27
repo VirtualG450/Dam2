dcf66c36c2ceeab763b4a7f4ddb7ff39b3580cf2
VirtualG450 <DilanMackey@hotmail.com>
2023-11-27 20:32:23 +0100

    ejercicio rapido bucles

---
+++ src/ProgramServiciosProcesos/Bucle.java
@@ -0,0 +1,15 @@
+package ProgramServiciosProcesos;
+
+public class Bucle extends Thread {
+
+    private String nombre;
+    public Bucle (String nombre) {
+        this.nombre=nombre;
+    }
+
+    public void run() {
+        for (int i = 0;i<5;i++) {
+            System.out.println("Proceso "+nombre+" en "+i);
+        }
+    }
+}
---
+++ src/ProgramServiciosProcesos/main5.java
@@ -0,0 +1,16 @@
+package ProgramServiciosProcesos;
+
+public class main5  {
+
+        public static void main(String[] args) {
+            
+            Bucle b1,b2;
+            b1 = new Bucle("primero");
+            b2 = new Bucle("segundo");
+            b1.start();
+            b2.start();
+
+        }
+
+    
+}
