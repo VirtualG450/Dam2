eb1c9fd99d110dc0649303e3904dfa72de84984d
VirtualG450 <DilanMackey@hotmail.com>
2023-11-27 21:08:31 +0100

    meh

--- src/ProgramServiciosProcesos/bucletestb.java
+++ src/ProgramServiciosProcesos/bucletestb.java
@@ -7,11 +7,17 @@ public class bucletestb extends Thread {
     public bucletestb(String nombre){
         this.nombre=nombre;
     }
-
     public synchronized void run(){
         while (bucletest.tareas < 100) {
             System.out.println("Hilo "+nombre+" añadio 1");
             bucletest.tareas++;
+            notifyAll();
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
         } System.out.println(nombre+" terminado con "+bucletest.tareas);
     }
 }
