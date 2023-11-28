43527b0d5b09245bf5f638dedeaf6f60796f70dc
VirtualG450 <DilanMackey@hotmail.com>
2023-11-28 02:07:58 +0100

    empezando caso practico psp 1UD2

--- src/ProgramServiciosProcesos/bucletest.java
+++ src/ProgramServiciosProcesos/bucletest.java
@@ -2,11 +2,12 @@ package ProgramServiciosProcesos;
 
 public class bucletest {
     public static int tareas = 0;
+    public static boolean puedoactuar = true;
 
     public static void main(String[] args) {
         bucletestb b1,b2;
-        b1=new bucletestb("primero");
-        b2=new bucletestb("segundo");
+        b1=new bucletestb("1");
+        b2=new bucletestb("2");
         b1.start();
         b2.start();
     }
--- src/ProgramServiciosProcesos/bucletestb.java
+++ src/ProgramServiciosProcesos/bucletestb.java
@@ -11,13 +11,6 @@ public class bucletestb extends Thread {
         while (bucletest.tareas < 100) {
             System.out.println("Hilo "+nombre+" añadio 1");
             bucletest.tareas++;
-            notifyAll();
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
         } System.out.println(nombre+" terminado con "+bucletest.tareas);
     }
 }
