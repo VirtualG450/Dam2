068115d9ad6abadfacda00694c1ae544e7d96d4c
VirtualG450 <DilanMackey@hotmail.com>
2024-02-13 19:01:31 +0100

    sysout error server

--- src/ProgramServiciosProcesos/UD3/CasoPractico1/Servidor.java
+++ src/ProgramServiciosProcesos/UD3/CasoPractico1/Servidor.java
@@ -8,15 +8,15 @@ public class Servidor {
     public static void main(String[] args) {
         
         //Creamos el Socket del servidor y le asignamos un puerto
-        System.out.println("Iniciando servidor");
         try {
+        System.out.println("Iniciando servidor");
             ServerSocket ss = new ServerSocket(8888);
             Socket soc = ss.accept();
 
             //Si la conexion se completa sin problemas aparecera el siguiente mensaje
             System.out.println("Conexion establecida");
         } catch (IOException e) {
-            // TODO Auto-generated catch block
+            System.out.println("Error en la conexion");
             e.printStackTrace();
         }
 
