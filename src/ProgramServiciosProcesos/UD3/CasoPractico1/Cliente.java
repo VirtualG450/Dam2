80d092fe88b4f4dfbbd4e88a240a14ab44c01415
VirtualG450 <DilanMackey@hotmail.com>
2024-02-13 18:07:17 +0100

    Variables y scanner

--- src/ProgramServiciosProcesos/UD3/CasoPractico1/Cliente.java
+++ src/ProgramServiciosProcesos/UD3/CasoPractico1/Cliente.java
@@ -1,5 +1,17 @@
 package ProgramServiciosProcesos.UD3.CasoPractico1;
+import java.util.Scanner;
 
 public class Cliente {
-    
+    //Variables necesarias para enviar al servidor y operacion a realizar
+    private int numero1,numero2;
+    private String operacion;
+    public static void main(String[] args) {
+        //Creamos el input del usuario usando scanner
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce el primer numero");
+        
+        //Solicitar al usuario los numeros y operacion a realizar que se almacenan en las variables
+
+        
+    }
 }
